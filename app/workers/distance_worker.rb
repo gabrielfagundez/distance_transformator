cca91d1066f0597840c90721022e03d189eeb69c
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-10 18:59:13 -0200

    Modificaciones en el worker Necesitamos decidir como implementar las consultas a Google

--- app/workers/distance_worker.rb
+++ app/workers/distance_worker.rb
@@ -1,8 +1,40 @@
 class DistanceWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
+  BASE_URL = 'http://maps.googleapis.com/maps/api/distancematrix/json?mode=driving&language=es&sensor=false'
+
+  # Este es el archivo de entrada
+  LOCATIONS_FILE = ''
+
+  # Este es el archivo de salida
+  DISTANCES_FILE = ''
+
+
+  # TODO: Procesamiento en segundo plano
   def perform
-    # Do something..
+
+    puts 'Iniciando proceso en segundo plano..'
+    # Borramos ejecucion previa del algoritmo si existe
+    begin
+      FileUtils.rm(DISTANCES_FILE)
+    rescue
+    end
+
+    # Leemos el archivo linea a linea
+    File.open(DISTANCES_FILE, 'r').each_line do |line|
+
+      # Imprimimos un asterisco por linea
+      print '*'
+
+      ubicacion = line.split(',')
+      latitud   = ubicacion[0]
+      longitud  = ubicacion[1]
+
+
+    end
+
+
   end
 
 end
