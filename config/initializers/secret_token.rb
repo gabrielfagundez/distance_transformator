2526e67a01226bfe754380de78b8293c73f719ad
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-11-10 18:31:45 -0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DistanceTransformator::Application.config.secret_key_base = 'e73b435e462b3d57662461aba588b2ce9e6597019060cdbdb488dcbb785ee9fd923de5ea7fbe06cd45aa789b8ea4dbfc06929ad86e380cbeb23ab3c93a372ad2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DistanceTransformator::Application.config.session_store :cookie_store, key: '_distance_transformator_session'
