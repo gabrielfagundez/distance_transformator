01c114f033c74b5a2d22edd61f600941539290d2
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2013-12-02 20:18:02 -0200

    Calculos finales

--- app/workers/distance_worker.rb
+++ app/workers/distance_worker.rb
@@ -6,16 +6,17 @@ class DistanceWorker
   PATH_URI  = '?'
   END_URI   = '&mode=driving&language=es&sensor=false'
 
+  TFF_ENTITY  = 'http://api.taxifarefinder.com/entity?key=d6apr3UDROuv&location='
+  TFF_CALCULO = 'http://api.taxifarefinder.com/fare?key=d6apr3UDROuv&'
+
   # Este es el archivo de entrada
-  LOCATIONS_FILE = 'entrada.txt'
+  ENTRADA_TXT = 'entrada.txt'
 
   # Este es el archivo de salida
-  DISTANCES_FILE  = 'salida.txt'
-  NAMES_FILE      = 'names.txt'
+  SALIDA_TXT  = 'salida.txt'
 
-  # Distancias
-  DISTANCES = []
-  NAMES     = []
+  # Este es el archivo de bandera
+  BANDERA_TXT  = 'bandera.txt'
 
   def perform
     
@@ -29,85 +30,206 @@ class DistanceWorker
     @dis_log.info 'Borrando archivo de resultados previo..'
     # Borramos ejecucion previa del algoritmo si existe
     begin
-      FileUtils.rm(DISTANCES_FILE)
+      FileUtils.rm(Rails.root + '/' + SALIDA_TXT)
     rescue
     end
 
     begin
-      FileUtils.rm(NAMES_FILE)
+      FileUtils.rm(Rails.root + '/' + BANDERA_TXT)
     rescue
     end
 
+    @costos   = []
+    @destinos = []
+    @bandera  = nil
+    @matriz_costos = nil
+
     # Leemos el archivo linea a linea
-    File.open(LOCATIONS_FILE, 'r').each_line do |line|
+    File.open(ENTRADA_TXT, 'r').each_line do |line|
 
       sanitized_line = line.split("\n")[0]
       @dis_log.info "Procesando linea #{contador}.."
 
-      # Obtenemos los parametros
-      parametros = "origins=#{ sanitized_line }&destinations=#{ DISTANCES.join('|') }"
-
-      unless DISTANCES.empty?
-
-        # Realizamos la consulta
-        http_response = HTTParty.get(BASE_URI + PATH_URI + parametros + END_URI)
-        json_response = JSON[http_response.body]
-
-        if json_response['status'] == 'OK'
-
-          # Obtenemos el nombre y lo almacenamos
-          NAMES.push(json_response['origin_addresses'][0])
-
-          # Obtenemos la distancia y la almacenamos
-          distances = []
-          json_response['rows'][0]['elements'].each do |destination|
-            if destination['status'] == 'OK'
-              distances.push(destination['distance']['value'])
-            else
-              @dis_log.info 'Ha ocurrido un error en la consulta puntual. El resultado es el siguiente: '
-              @dis_log.info destination.to_hash.inspect
-              @dis_log.info 'Para mas informacion: https://developers.google.com/maps/documentation/distancematrix/'
-              @dis_log.info 'Las causas pueden ser: NOT_FOUND > La posicion no se puede localizar. ZERO_RESULTS No existe una ruta entre origen y destino.'
-              return
-            end
-          end
-          DISTANCES.push(distances)
-
-        else
-          @dis_log.info 'Ha ocurrido un error en la consulta:'
-          @dis_log.info json_response.inspect
-          @dis_log.info 'Para mas informacion: https://developers.google.com/maps/documentation/distancematrix/'
-          @dis_log.info 'Las causas pueden ser: '
-          @dis_log.info 'INVALID_REQUEST > Consulta erronea.'
-          @dis_log.info 'MAX_ELEMENTS_EXCEEDED > Se excede el maximo por consulta.'
-          @dis_log.info 'OVER_QUERY_LIMIT > Se han recibido muchas consultas desde esta IP en un corto periodo de tiempo.'
-          @dis_log.info 'REQUEST_DENIED > Se prohibe el acceso de este sitio.'
-          return
-        end
-
-      else
-        DISTANCES.push([])
-      end
-
-      # Guardamos la linea
-      DISTANCES.push(sanitized_line)
+      calcular_costo(sanitized_line)
 
       # Actualizamos el contador
       contador = contador + 1
 
       @dis_log.info "Linea #{contador} procesada.."
-
     end
+
+    armar_matriz_costos
+    crear_archivo_de_bandera
 
     # Imprimimos el final
     @dis_log.info 'El arreglo de distancias tiene el siguiente aspecto:'
-    @dis_log.info DISTANCES.inspect
+    @dis_log.info @destinos.inspect
 
-    @dis_log.info 'Los nombre calculados tienen el siguiente aspecto:'
-    @dis_log.info NAMES.inspect
+    @dis_log.info 'El arreglo de costos tiene el siguiente aspecto:'
+    @dis_log.info @costos.inspect
+
+    @dis_log.info 'La matriz de costos tiene el siguiente aspecto:'
+    @dis_log.info @matriz_costos.inspect
+
+    @dis_log.info 'La bandera es:'
+    @dis_log.info @bandera.inspect
+
+    crear_archivo_de_costos
 
     @dis_log.info 'Se ha finalizado el proceso en segundo plano..'
 
+    @dis_log.info '***********************************************'
+    @dis_log.info '***********************************************'
+    @dis_log.info '***********************************************'
+
+  end
+
+  def calcular_costo(string_origen)
+
+    if @destinos.empty?
+
+      # Realizamos la consulta
+      url = TFF_ENTITY + string_origen
+
+      @dis_log.info url
+
+      http_response = HTTParty.get(url)
+      json_response = JSON[http_response.body]
+
+      @dis_log.info json_response.inspect
+
+      @entity = json_response['handle']
+      @destinos.push(string_origen)
+      @costos.push([])
+    else
+
+      costos = []
+
+      @destinos.each do |destino|
+
+        parametros = {
+            entity_handle:  @entity,
+            origin:         string_origen,
+            destination:    destino
+        }
+
+        # Realizamos la consulta
+        http_response = HTTParty.get(TFF_CALCULO + parametros.to_param)
+        json_response = JSON[http_response.body]
+
+        @dis_log.info json_response.inspect
+
+        costos.push(json_response['metered_fare'])
+        @bandera = json_response['initial_fare']
+      end
+
+      @costos.push(costos)
+      @destinos.push(string_origen)
+    end
+  end
+
+  def armar_matriz_costos
+
+    cantidad_marcadores = @costos.size
+
+    @dis_log.info 'Calculando la matriz de costos..'
+    @dis_log.info 'Se tienen en cuenta ' + cantidad_marcadores.to_s + ' marcadores.'
+
+    # Matriz de costos
+    @matriz_costos  = Array.new(cantidad_marcadores) { Array.new(cantidad_marcadores) }
+
+    # Rellenamos la matriz de costos
+    for i in 0..(cantidad_marcadores - 1) do
+      for j in 0..(cantidad_marcadores - 1) do
+        if i == j
+          @matriz_costos[i][j] = 0
+        else
+          if i>j
+            @matriz_costos[i][j] = @costos[i][j]
+          else
+            @matriz_costos[i][j] = @costos[j][i]
+          end
+        end
+      end
+    end
+  end
+
+  def crear_archivo_de_costos
+    @dis_log.info 'Creando archivo de costos en ' + Rails.root.to_s + '/' + SALIDA_TXT + '..'
+
+    cantidad_marcadores = @costos.size
+
+    `touch #{Rails.root.to_s + '/' + SALIDA_TXT}`
+
+    File.open(Rails.root.to_s + '/' + SALIDA_TXT, 'w') do |file|
+      for i in 0..(cantidad_marcadores - 1) do
+        for j in 0..(cantidad_marcadores - 1) do
+          file.write @matriz_costos[i][j]
+          file.write ' '
+        end
+        file.write "\n"
+      end
+    end
+
+    @dis_log.info 'Archivo de costos creado.'
+  end
+
+  def crear_archivo_de_bandera
+    @dis_log.info 'Creando archivo de bandera en ' + Rails.root.to_s + '/' + BANDERA_TXT + '..'
+
+    cantidad_marcadores = @costos.size
+
+    `touch #{Rails.root.to_s + '/' + BANDERA_TXT}`
+
+    File.open(Rails.root.to_s + '/' + BANDERA_TXT, 'w') do |file|
+      file.write @bandera.to_s
+      file.write "\n"
+    end
+
+    @dis_log.info 'Archivo de bandera creado.'
+  end
+
+
+
+
+
+
+  def metodo_deprecado
+    ## Realizamos la consulta
+    #http_response = HTTParty.get(BASE_URI + PATH_URI + parametros + END_URI)
+    #json_response = JSON[http_response.body]
+    #
+    #if json_response['status'] == 'OK'
+    #
+    #  # Obtenemos el nombre y lo almacenamos
+    #  NAMES.push(json_response['origin_addresses'][0])
+    #
+    #  # Obtenemos la distancia y la almacenamos
+    #  distances = []
+    #  json_response['rows'][0]['elements'].each do |destination|
+    #    if destination['status'] == 'OK'
+    #      distances.push(destination['distance']['value'])
+    #    else
+    #      @dis_log.info 'Ha ocurrido un error en la consulta puntual. El resultado es el siguiente: '
+    #      @dis_log.info destination.to_hash.inspect
+    #      @dis_log.info 'Para mas informacion: https://developers.google.com/maps/documentation/distancematrix/'
+    #      @dis_log.info 'Las causas pueden ser: NOT_FOUND > La posicion no se puede localizar. ZERO_RESULTS No existe una ruta entre origen y destino.'
+    #      return
+    #    end
+    #  end
+    #  DISTANCES.push(distances)
+    #
+    #else
+    #  @dis_log.info 'Ha ocurrido un error en la consulta:'
+    #  @dis_log.info json_response.inspect
+    #  @dis_log.info 'Para mas informacion: https://developers.google.com/maps/documentation/distancematrix/'
+    #  @dis_log.info 'Las causas pueden ser: '
+    #  @dis_log.info 'INVALID_REQUEST > Consulta erronea.'
+    #  @dis_log.info 'MAX_ELEMENTS_EXCEEDED > Se excede el maximo por consulta.'
+    #  @dis_log.info 'OVER_QUERY_LIMIT > Se han recibido muchas consultas desde esta IP en un corto periodo de tiempo.'
+    #  @dis_log.info 'REQUEST_DENIED > Se prohibe el acceso de este sitio.'
+    #  return
+    #end
   end
 
 end
